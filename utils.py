f2f97c0900ebded4bd185afd44eb11b914cc1c2b
Manish  Sharma <manish.sharma17@imperial.ac.uk>
2018-05-05 12:30:37 +0100

    tokenize_fun

---
+++ utils.py
@@ -0,0 +1,25 @@
+import nltk
+import re
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+ps = PorterStemmer()
+def tokenize(phrase):
+	# Remove any punctuation
+	phrase = re.sub(r'[^\w\s]','',phrase)
+	print(phrase)
+	lst = [];
+	for word in phrase.split():
+		word = ps.stem(word)
+		if(len(word)>2):
+			lst.append(word)
+	return ",".join(lst)
+
+def testTokenize():
+	text = "Tandoori Roti (d) (Wheat)"
+	text2 = "breakfast, eggs, juices, Lunch, milkshakes, "
+	print(tokenize(text))
+	print(tokenize(text2))
+	print(tokenize("potatoes"))
+
+testTokenize()
